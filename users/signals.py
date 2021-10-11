759f840c016bbc22158be9c9ec73463f52460cf9
prathamesh <prathamesh19p@gmail.com>
2021-10-11 22:03:20 +0530

    update signal

--- users/signals.py
+++ users/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from .models import Profile
 
 
-# signal that gets fired after the user is saved
+# signal which gets fired after the user is saved
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
